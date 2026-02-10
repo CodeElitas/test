d599dabf11c24b57e5a7e8219ab4b9a8b9986415
CodeElitas <rasloan3@gmail.com>
2026-02-10 18:11:08 -0500

    Update project.c

--- project.c
+++ project.c
@@ -56,7 +56,7 @@ int main()
 
     // Make the random number less
     // than 100, divided it by 100
-    n = rand() % 100;
+    
 
     // Using simple probability 100 is
     // roughly divided among stone,
